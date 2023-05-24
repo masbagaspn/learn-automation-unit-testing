e92803187451d94751c6aee34044a7a265025735
masbagaspn <masbagaspn@gmail.com>
2023-05-25 01:40:32 +0700

    feat: testing using queries getByLabelText

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -5,23 +5,37 @@ describe('Application', () => {
   test('render correctly', () => {
     render(<Application />);
 
+    // queries: getByRole, option: level
     const pageHeading = screen.getByRole('heading', { level: 1 });
     expect(pageHeading).toBeInTheDocument();
 
     const sectionHeading = screen.getByRole('heading', { level: 2 });
     expect(sectionHeading).toBeInTheDocument();
 
+    // queries: getByRole, options: name
     const nameElement = screen.getByRole('textbox', { name: 'Name' });
     expect(nameElement).toBeInTheDocument();
+
+    // queries: getByLabelTest, options: selector
+    const secondNameElement = screen.getByLabelText('Name', {
+      selector: 'input',
+    });
+    expect(secondNameElement).toBeInTheDocument();
 
     const bioElement = screen.getByRole('textbox', { name: 'Bio' });
     expect(bioElement).toBeInTheDocument();
 
+    // queries: getByRole, options: default
     const jobLocationElement = screen.getByRole('combobox');
     expect(jobLocationElement).toBeInTheDocument();
 
     const termsElement = screen.getByRole('checkbox');
     expect(termsElement).toBeInTheDocument();
+
+    const secondTermsElement = screen.getByLabelText(
+      'I agree to terms and conditions'
+    );
+    expect(secondTermsElement).toBeInTheDocument();
 
     const submitButtonElement = screen.getByRole('button');
     expect(submitButtonElement).toBeInTheDocument();
