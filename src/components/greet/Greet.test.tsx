535cfc53b2fdc0e140dfd7c86fc68ff3ace06683
masbagaspn <masbagaspn@gmail.com>
2023-05-24 18:54:13 +0700

    automation by grouping test name using describe

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -6,14 +6,16 @@
 import { render, screen } from '@testing-library/react';
 import { Greet } from './Greet';
 
-test('Greet renders correctly', () => {
-  render(<Greet />);
-  const textElement = screen.getByText('Hello');
-  expect(textElement).toBeInTheDocument();
-});
+describe('Greet', () => {
+  test('renders correctly', () => {
+    render(<Greet />);
+    const textElement = screen.getByText('Hello');
+    expect(textElement).toBeInTheDocument();
+  });
 
-test('Greet renders with a name', () => {
-  render(<Greet name="Firza" />);
-  const textElement = screen.getByText('Hello Firza');
-  expect(textElement).toBeInTheDocument();
+  test('renders with a name', () => {
+    render(<Greet name="Firza" />);
+    const textElement = screen.getByText('Hello Firza');
+    expect(textElement).toBeInTheDocument();
+  });
 });
